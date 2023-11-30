55f9feb88fd3aae105260a08fccefaabe88c7664
Kelvin Guerrero <kelvinguerrero2199@outlook.es>
2023-11-30 23:16:51 +0100

    Chirp controller creado, index de los chirps actualizado, mensaje de sesion al crear un nuevo chirp creado, migraciones de los chirps creadas

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use Illuminate\Http\Request;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('chirps.index');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Chirp::create([
+            'message' => $request->get('message'),
+            'user_id' => auth()->id(),
+        ]);
+
+        return to_route('chirps.index')
+            ->with('status', __('Chirp created successfully!'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Chirp $chirp)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -31,12 +31,11 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/chirps', function () {
-        return view('chirps.index');
-    })->name('chirps.index');
-    Route::post('/chirps', function () {
-        return 'Processing Chirp...';
-    });
+    Route::get('/chirps', [ChirpController::class, 'index'])
+        ->name('chirps.index');
+
+    Route::post('/chirps', [ChirpController::class, 'store'])
+        ->name('chirps.store');
 });
 
 require __DIR__ . '/auth.php';
